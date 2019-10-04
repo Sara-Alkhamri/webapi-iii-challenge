048c767116722acf2364be00e907befe502a162a
Sara Alkhamri <s.alkhamri@gmail.com>
2019-10-03 18:30:43 -0700

    updates

--- knexfile.js
+++ knexfile.js
@@ -17,5 +17,24 @@ module.exports = {
     seeds: {
       directory: './data/seeds',
     },
+
   },
+  production: {
+    client: "pg", // need to npm i pg
+    connection: {
+      filename: './data/blog.db3',
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    pool: {
+      min: 2,
+      max: 100 // this is to ensure the migration will not time out
+    }
+  }
 };
+  
+
