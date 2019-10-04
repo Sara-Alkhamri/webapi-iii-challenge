e3acc3184dbb369f421b7a3ed4aeae84af44eb9f
Sara Alkhamri <s.alkhamri@gmail.com>
2019-10-03 17:48:05 -0700

    Updates to index.js

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const server = require('./server');
 
 // const defaults = require('./config/defaults');
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT;
 
 // server.listen(defaults.port, () => 
 //     console.log(`\n** API Ruuning on ${defaults.port} **\n`))
-
+// var server = app.listen(process.env.PORT || 5000, function () {
+//  var port = server.address().port;
+//  console.log("Express is working on port " + port);
+// });
 
 server.listen(port, () => 
 console.log(`\n** API Ruuning on ${port} **\n`))    
